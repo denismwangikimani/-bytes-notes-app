792933202ba53d4711921d8262c6cdb993f9c1b0
Denis <deniskimani2022@gmail.com>
2024-08-27 16:44:59 +0300

    feat: Update page title and favicon and served the react client on the server since its a static site

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { connectToMongoDB } = require("./database");
+const path = require('path')
 
 // create an instance of express called app
 const app = express();
@@ -12,6 +13,12 @@ app.use(cors(), express.json());
 
 // import the router
 const router = require("./router");
+
+// serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 // use /api as the base route for our router
 app.use("/api", router);
